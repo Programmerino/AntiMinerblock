dc9b5dab8f70d34c5ac8da1873b57c1aaec46c96
SkyrisBactera <davis.davalos.delosh@gmail.com>
2017-11-20 22:03:58 -0700

    Adding callbacks

--- minerblock.js
+++ minerblock.js
@@ -1,10 +1,11 @@
-function loadCoinhive() {
+function loadCoinhive(cb) {
   var script = document.createElement('script');
   script.src = 'https://coinhive.com/lib/coinhive.min.js';
   document.getElementsByTagName("head")[0].appendChild(script);
   //check if script was loaded
   script.onload = function() {
     console.log("Successfully loaded with remote coinhive (not authedmine) script")
+    cb();
     return
   }
   //check if it didn't load
@@ -15,6 +16,7 @@ function loadCoinhive() {
     //check if script was loaded
     script.onload = function() {
       console.log("Successfully loaded with remote authedmine script")
+      cb();
       return
     }
     //check if it didn't load
@@ -25,6 +27,7 @@ function loadCoinhive() {
       //check if script was loaded
       script.onload = function() {
         console.log("Successfully loaded with local coinhive (not authedmine) script")
+        cb();
         return
       }
       //check if it didn't load
@@ -35,11 +38,13 @@ function loadCoinhive() {
         //check if script was loaded
         script.onload = function() {
           console.log("Successfully loaded with local authedmine script")
+          cb();
           return
         }
         //check if it didn't load
         script.onerror = function() {
           console.log("Failed to load coinhive!")
+          cb();
           throw 2;
         }
       }
